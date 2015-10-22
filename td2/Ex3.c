170cf1f07aa05240511178b8a979e62f6d948909
yobero <yoannb94@gmail.com>
2015-10-22 11:21:40 +0200

    Ex3 Qc

--- td2/Ex3.c
+++ td2/Ex3.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define N 10
+
 struct element {
 	int val;
 	struct element* suiv;
@@ -7,15 +9,26 @@ struct element {
 typedef struct element Element;
 typedef Element* liste;
 
+int alea ()
+{
+	int valeur;
+		valeur=rand()%N;
+		return valeur;
+}
+
 liste creerListeVides(liste l)
 {
 	return NULL;
 }
 
-void listeVide(liste l)
+int listeVide(liste l)
 {
 	if (l == NULL)
+	{
 		printf("La liste est vide \n");
+		return 0;
+	}
+	 return 1;
 }
 
 void afficheListe (liste l)
@@ -27,9 +40,55 @@ void afficheListe (liste l)
 	}
 }
 
-liste ajout(liste l)
+liste tri(liste l)
 {
-	liste x;
+	liste sauv=l;
+	liste p=l->suiv;
+	int tmp;
+	
+	while(l != NULL)
+	{
+		while(p!=NULL)
+		{
+			if (l->val > p->val)
+			{
+				tmp= l->val;
+				l->val = p->val;
+				p->val = tmp;
+				p=l;
+			}
+			p=p->suiv;
+		}
+		l=l->suiv;
+	}
+	return sauv;
+}
+
+liste ajoutDebut(liste l)
+{
+	liste elt = malloc(sizeof(liste));
+	int t;
+	
+	printf("Choisir une valeur a ajouter dans la liste ");
+	scanf("%d",&t);
+	
+	elt->val = t;
+	elt->suiv =l;
+	
+	return elt;
+}
+
+liste ajoutFin(liste l)
+{
+	liste elt = malloc(sizeof(liste));
+	int t;
+	
+	printf("Choisir une valeur a ajouter dans la liste ");
+	scanf("%d",&t);
+	
+	elt->val = t;
+	elt->suiv = NULL;
+	l->suiv = elt ;
 	
 	return l;
 }
@@ -44,13 +103,27 @@ int recherche (liste l,int e) //e la valeur à rechercher
 		return recherche(l->suiv,e);
 }
 
+int nombreElements(liste l)
+{
+	int compteur=0;
+	while(l!=NULL)
+	{
+		l=l->suiv;
+		compteur++;
+	}
+	return compteur;
+}
+
 int main ()
 {
 	liste l;
+	int compteur;
 	
 	l=creerListeVides(l);
-	listeVide(l);
+	l= ajoutDebut(l);
 	afficheListe(l);
+	
+	compteur=nombreElements(l);
 	
 	return 0;
 }
