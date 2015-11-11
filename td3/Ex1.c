805c24de1eba7ab2b591a05f7dfe8c8d73f77d2e
yobero <yoannb94@gmail.com>
2015-11-11 19:33:49 +0100

    Ajout Exe.c

--- td3/Ex1.c
+++ td3/Ex1.c
@@ -3,9 +3,21 @@
 #include <time.h>
 #include <string.h>
 
+/* argc un tableau de chaine de caractère
+ * argc la taille du tableau */
+///Voir une autre ligne de commande pour windows.
+
 int main( int argc, char** argv)
 {
 	int i;
 	for(i=0; i<argc; i++)
-		printf("%s\n", argv[i]); return 0;
+		printf("%s\n", argv[i]);
+	return 0;
 }
+
+/* De base un talbeau est un pointeur donc char* mais argv est un pointeur du tableau
+ * donc pointeur de pointeur d'ou char**. */
+
+/* faire un main 3 atof (si ça marche) sur le PC. */
+
+/* Faire de même pour cette question. */
