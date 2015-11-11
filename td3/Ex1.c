6093b5f2057e087829ba678b4e397ed270e4414b
yobero <yoannb94@gmail.com>
2015-11-11 19:17:15 +0100

    Ajout du dossier td3 + ex1.c

---
+++ td3/Ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+int main( int argc, char** argv)
+{
+	int i;
+	for(i=0; i<argc; i++)
+		printf("%s\n", argv[i]); return 0;
+}
