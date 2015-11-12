4d5a0496c07e5d5fa5383f5743599f5097c14df2
Utilisateur <user@lubuntu-13.04>
2015-11-12 12:09:11 +0100

    Qd

--- td3/Ex1.c
+++ td3/Ex1.c
@@ -2,21 +2,39 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <ctype.h>
 
 /* argc un tableau de chaine de caractère
  * argc la taille du tableau */
-///Voir une autre ligne de commande pour windows.
+
+int test(char* x)
+{
+	int t;
+	
+	t=isdigit(x);
+	return t;
+}
 
 int main( int argc, char** argv)
 {
+	double som=0; ///La fonction atof renvoie un double.
+	int t;
+	
 	int i;
+	
 	for(i=0; i<argc; i++)
+	{
 		printf("%s\n", argv[i]);
+		t=test(argv[i]);
+		printf("T vaut %d \n",t);
+		if(t)
+			som = som + atof(argv[i]); /// Fonctionnne seulement si argbv[i] est un nombre.
+	}
+	printf("somme = %lf \n",som);
 	return 0;
 }
 
-/* De base un talbeau est un pointeur donc char* mais argv est un pointeur du tableau
- * donc pointeur de pointeur d'ou char**. */
+/* char** est un tableau de chaine de caractère. */
 
 /* faire un main 3 atof (si ça marche) sur le PC. */
 
