8e41b39d50076692cbdeec99856caa060d732232
yobero <yoannb94@gmail.com>
2015-11-12 12:48:11 +0100

    Ex1 terminer

--- td3/Ex1.c
+++ td3/Ex1.c
@@ -9,25 +9,28 @@
 
 int test(char* x)
 {
-	int t;
-	
-	t=isdigit(x);
-	return t;
+	int t=1;
+	int i=0;
+	while (x[i] != '\0')
+	{
+		if (isdigit(x[i]) || x[i]=='.' || x[i]=='-')
+			i++;
+		else
+			return 0;
+	}
+	return 1;
 }
 
 int main( int argc, char** argv)
 {
 	double som=0; ///La fonction atof renvoie un double.
-	int t;
 	
 	int i;
 	
 	for(i=0; i<argc; i++)
 	{
 		printf("%s\n", argv[i]);
-		t=test(argv[i]);
-		printf("T vaut %d \n",t);
-		if(t)
+		if(test(argv[i]))
 			som = som + atof(argv[i]); /// Fonctionnne seulement si argbv[i] est un nombre.
 	}
 	printf("somme = %lf \n",som);
