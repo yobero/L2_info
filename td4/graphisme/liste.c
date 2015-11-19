99c30f5da978775689e7598d39e4ce4600ea90a6
yobero <yoannb94@gmail.com>
2015-11-19 13:51:20 +0100

    Ex3

---
+++ td4/graphisme/liste.c
@@ -0,0 +1,42 @@
+#include "graphics.h"
+
+#define COUL_FOND  noir
+
+struct element{
+  POINT p;
+  struct element *suiv;
+};
+typedef struct element *Liste;
+
+Liste ajoutDebut(Liste l, POINT p){
+  Liste tmp = malloc(sizeof(struct element));
+  tmp->p = p;
+  tmp->suiv = l;
+  return tmp;
+}
+
+Liste supprimeDebut(Liste l){
+  Liste ll;
+  if(l != NULL){
+    ll = l;
+    l = l->suiv;
+    free(ll);
+  }
+  return l;
+}
+
+
+void dessineListeRec(Liste l){
+  if(l != NULL){
+    draw_fill_circle(l->p, 5, bleu);
+    if(l->suiv != NULL)
+      draw_line(l->p, l->suiv->p, rouge);
+    dessineListeRec(l->suiv);
+  }
+}
+
+void dessineListe(Liste l){
+  fill_screen(COUL_FOND);
+  dessineListeRec(l);
+  affiche_all();
+}
--- td4/graphisme/main.c
+++ td4/graphisme/main.c
@@ -1,4 +1,6 @@
 #include "graphics.h"
+#include "liste.c"
+#include "simul.c"
 
 // ############################
 // 1. Les structures de données
@@ -6,15 +8,6 @@
 
 #define LARGEUR    800
 #define HAUTEUR    800
-#define COUL_FOND  noir
-#define NB_POINTS  20
- 
-
-struct element{
-  POINT p;
-  struct element *suiv;
-};
-typedef struct element *Liste;
 
 
 // ############################
@@ -22,62 +15,9 @@ typedef struct element *Liste;
 // ############################
 
 
-Liste ajoutDebut(Liste l, POINT p){
-  Liste tmp = malloc(sizeof(struct element));
-  tmp->p = p;
-  tmp->suiv = l;
-  return tmp;
-}
-
-Liste supprimeDebut(Liste l){
-  Liste ll;
-  if(l != NULL){
-    ll = l;
-    l = l->suiv;
-    free(ll);
-  }
-  return l;
-}
-
-
-void dessineListeRec(Liste l){
-  if(l != NULL){
-    draw_fill_circle(l->p, 5, bleu);
-    if(l->suiv != NULL)
-      draw_line(l->p, l->suiv->p, rouge);
-    dessineListeRec(l->suiv);
-  }
-}
-
-void dessineListe(Liste l){
-  fill_screen(COUL_FOND);
-  dessineListeRec(l);
-  affiche_all();
-}
-
 // ############################
 // 3. Simulation
 // ############################
-
-
-void simulChaine(){
-  Liste l = NULL;
-  int i = 0;
-  char c;
-  POINT p;
-  while(i < NB_POINTS){
-    p = wait_key_or_clic(&c);
-    if(c == 0){//ajoute un point
-      l = ajoutDebut(l, p);
-      i++;
-    }
-    else if(c == 'a'){//annule le dernier point
-      l = supprimeDebut(l);
-      i--;
-    }
-    dessineListe(l);
-  }
-}
 
 
 
@@ -95,7 +35,7 @@ int main(){
 
   simulChaine();
 
-
+	printf("%d \n",NB_POINTS);
 
   wait_escape();
   return 0;
