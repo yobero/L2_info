37c98e015ccd576eeec853decbbd1329cd4d0d4b
yobero <yoannb94@gmail.com>
2015-11-26 14:38:55 +0100

    Ajout d'une constance NBM

--- projet/constante.c
+++ projet/constante.c
@@ -4,6 +4,7 @@
 #define l  720//taille de la fenetre (un mutliple de 9)
 #define TAILLE l/9 //la taille de la case
 #define PM  TAILLE/2 //distance Pion-Mur
+#define NBM 10 //nb de murs
 
 //contante pour couleur
 #define GRILLE white
