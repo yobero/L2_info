bb2bb11dd1e1cdce5b13eda0de114112589963ef
yobero <yoannb94@gmail.com>
2015-11-27 12:32:20 +0100

    Enumeration des fonction dans fonction.h

--- projet/fonction.h
+++ projet/fonction.h
@@ -7,3 +7,9 @@ PION initialisationPion(int a);
 MUR initialisationMur(int a);
 
 JOUEUR initialisationJoueur(int a);
+
+void affichagePion(PION p1,PION p2);
+
+void affichageMur(MUR m1, MUR m2);
+
+void affichageJoueur(JOUEUR j1,JOUEUR j2);
