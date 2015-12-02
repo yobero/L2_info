4d7916ff2c50abb3abcc988bfb714870ca5b7e5a
yobero <yoannb94@gmail.com>
2015-12-02 20:58:43 +0100

    Position vertical du mur FAIT

--- projet/fonction.c
+++ projet/fonction.c
@@ -203,7 +203,7 @@ int nbMaxMur(MUR m)
 	return 1;
 }
 
-POINT localisationClic(POINT utilisateur)
+POINT localisationClic(POINT utilisateur, int* HV)
 {
 	POINT p;
 	int i,j;
@@ -212,13 +212,21 @@ POINT localisationClic(POINT utilisateur)
 	
 	while (i<l)
 	{
-		if ((utilisateur.x > i-ED) && (utilisateur.x < i+TAILLE))
+		if ((utilisateur.x > i+ED) && (utilisateur.x < i+(TAILLE-ED)))
 			p.x = i;
+		else
+		{
+			if ((utilisateur.x > i-ED) && (utilisateur.x < i+ED))
+			{
+			p.x = i;
+			*HV = 0;
+			}
+		}
 		i=i+TAILLE;
 	}
 	while (j<l)
 	{
-		if (utilisateur.y > j-ED && utilisateur.y < j+ED)
+		if (utilisateur.y > j-ED && utilisateur.y < j+TAILLE)
 			p.y = j-ED;
 		j=j+TAILLE;	
 	}
@@ -242,7 +250,7 @@ int rechercheMur(MUR m)
 	return recherche;
 }
 
-MUR placementMur(POINT p, POINT utilisateur, MUR m)
+MUR placementMur(POINT p, POINT utilisateur, MUR m)//INUTILE	
 {
 	int recherche;
 	
@@ -257,18 +265,31 @@ MUR placementMur(POINT p, POINT utilisateur, MUR m)
 
 MUR ajoutMur (MUR m, POINT utilisateur)
 {
+	int HV=1;
+	printf("Avant %d\n",HV);
 	if (nbMaxMur(m))
 	{
 		POINT p;
 		
-		p=localisationClic(utilisateur);
-		//m=placementMur(p,utilisateur,m);
-		printf("Apres %d et %d\n",p.x,p.y);
-		m.tab[m.i][0].x = p.x;
-		m.tab[m.i][0].y = p.y;
-		m.tab[m.i][1].x = p.x + LONGM;
-		m.tab[m.i][1].y = p.y + LARGM;
-		m.i=m.i+1;
+		p=localisationClic(utilisateur,&HV);
+		printf("Apres %d\n",HV);
+		if (HV)
+		{
+			//m=placementMur(p,utilisateur,m);
+			m.tab[m.i][0].x = p.x+ED;
+			m.tab[m.i][0].y = p.y;
+			m.tab[m.i][1].x = p.x + LONGM-ED;
+			m.tab[m.i][1].y = p.y + LARGM;
+			m.i=m.i+1;
+		}
+		else
+		{
+			m.tab[m.i][0].x = p.x-ED;
+			m.tab[m.i][0].y = p.y+EPAISSEUR;
+			m.tab[m.i][1].x = m.tab[m.i][0].x+ LARGM;
+			m.tab[m.i][1].y = m.tab[m.i][0].y + LONGM-EPAISSEUR;
+			m.i=m.i+1;
+		}
 	}
 	else
 		printf("Tu ne peux plus placer de mur.\n"); //CHANGER ET AFFICHER DANS LA FENETRE
