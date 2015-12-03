cb3bd64583237881216962002f91e74f331661e9
Utilisateur <yobero>
2015-12-03 11:54:21 +0100

    Ex5 fini

--- td5/Ex5/ecrireTexte.c
+++ td5/Ex5/ecrireTexte.c
@@ -4,7 +4,36 @@
 
 int main ()
 {
+	char lol=97;
+	int n=0;
+	FILE* fichier = fopen("affichage.txt","w"); 
+	
+	//lettre minuscule
+	while (lol < 123)
+	{
+		fprintf(fichier,"%c",lol);
+		lol++;
+	}
+	fprintf(fichier,"\n");
+	lol=65;
+	//lettre majuscule
+	while (lol<91)
+	{
+		fprintf(fichier,"%c",lol);
+		lol++;
+	}
+	printf("\n");
+	
+	//Nombre
+	while (n<=999)
+	{
+		fprintf(fichier, "%4d", n);
+		if (n%10 == 0)
+			fprintf(fichier, "\n");
+		n++;
+	}
 	
 	
+	fclose(fichier);
 	return 0;
 }
