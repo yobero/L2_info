78997fb0ca63f21e7c1dcb1efcc3c31e70571eb7
Utilisateur <yobero>
2015-12-03 11:09:57 +0100

    cryptage non fini

--- td5/cryptographie/crypt.c
+++ td5/cryptographie/crypt.c
@@ -12,17 +12,21 @@ int main(int argc, char** argv)
 	
 	int val=0;
 	
+	printf("%d\n",fgets(str, 256, fichier));
+	printf("%d\n",fgets(str, 256, fichier));
+	printf("%d\n",fgets(str, 256, fichier));
+	
 	for(i=0;i<256;i++)
 	{
-		fgets(str, 256, fic);
+		fgets(str, 256, fichier);
 		r = atoi(str); ///converti une chaine de caractère en entier
-		fgets(str, 256, fic);
-		g = atoi(str);
-		fgets(str, 256, fic);
+		fgets(str, 256, fichier);
+		v = atoi(str);
+		fgets(str, 256, fichier);
 		b = atoi(str);
 		r = (r-r%4) + (argv[2][i]*64)%4 ;
 		v = (v-v%8) + (argv[2][i]*32)%8;
-		b = (b-b%8) + argc[2][i]%8);
+		b = (b-b%8) + (argv[2][i]%8);
 		
 		printf("%d \n %d \n %d",r,v,v);
 	}
