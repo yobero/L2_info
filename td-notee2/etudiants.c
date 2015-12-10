e9977a73fc96e6a3663ed8567a1a30b6ea4e18f6
yobero <yoannb94@gmail.com>
2015-12-10 10:08:31 +0100

    AJout de la fonction ajout

--- td-notee2/etudiants.c
+++ td-notee2/etudiants.c
@@ -4,23 +4,61 @@
 #include <sys/time.h>
 
 #define NMAX 100000
-	
-struct timeval {
-	
-};
 
 struct abr {
 	int val;
 	struct abr* fg;
 	struct abr* fd;
 };
-typedef struct abr ABR;
+typedef struct abr* ABR;
 
 
 int alea(int n){
   return (int)(1.0*n*rand()/RAND_MAX);
 }
 
+ABR initialisation()
+{
+	return NULL;
+}
+
+ABR ajoutElement(ABR a, int element)
+{
+	if (a == NULL)
+	{
+		ABR tmp = malloc(sizeof(ABR));
+	
+		tmp->val = element;
+		tmp->fg = NULL;
+		tmp->fd = NULL;
+		
+		return tmp;
+	}
+	else
+	{
+		if(element >= a->val)
+			a->fd = ajoutElement(a->fd,element);
+		else
+			a->fg = ajoutElement(a->fg,element);
+	}
+	return a;
+}
+
+ABR remplirABR(ABR a);
+{
+	int i=0;
+	int element;
+	
+	while (i<NMAX)
+	{
+		element = alea();
+		a=ajoutElement(a,element);
+		
+		i++;
+	}
+	
+	return a;
+}
 
 int main(void){
   struct timeval tv1,tv2;
