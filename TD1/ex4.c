01b9c8f1f4ba5f2567f0eb88f5201ca502ddba95
Utilisateur <yoannb94@gmail.com>
2016-02-03 12:16:44 +0100

    Ex4 terminer

--- TD1/ex4.c
+++ TD1/ex4.c
@@ -3,4 +3,27 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+int gc()
+{
+	int n;
+	unsigned char buf;
+	
+	if(read(0,&buf,1)>0)
+	{
+		n = buf;
+	}
+	else
+	{
+		n = EOF;
+	}
+	//pour savoir la valeur
+	printf("%d \n",n);
+	
+}
 
+int main ()
+{
+	gc();
+	
+	return 1;
+}
