5b6602ff1ca10820b9a5dd690fcb6522cf1190a6
yobero <yoannb94@gmail>
2016-02-09 21:04:27 +0100

    ex6.c correction

--- TD1/ex6.c
+++ TD1/ex6.c
@@ -6,21 +6,52 @@
 
 int main (int argc,const char** argv)
 {
+	//OK(partie 1)
 	struct stat buf;
 	
 	stat(argv[1],&buf);
 	
-	 switch (buf.st_mode & S_IFMT) {
-    case S_IFBLK:  printf("périphérique de bloc\n");      break;
-    case S_IFCHR:  printf("périphérique de caractère\n"); break;
-    case S_IFDIR:  printf("répertoire\n");                break;
+	//Montre le type du fichier demandé en utilisant ST_MODE
+	printf("Le fichier est ");
+	switch (buf.st_mode & S_IFMT) {
+    case S_IFBLK:  printf("un périphérique de bloc\n");      break;
+    case S_IFCHR:  printf("un périphérique de caractère\n"); break;
+    case S_IFDIR:  printf("un répertoire\n");                break;
     case S_IFIFO:  printf("FIFO/tube\n");                 break;
-    case S_IFLNK:  printf("lien symbolique\n");           break;
-    case S_IFREG:  printf("fichier ordinaire\n");         break;
-    case S_IFSOCK: printf("socket\n");                    break;
+    case S_IFLNK:  printf("un lien symbolique\n");           break;
+    case S_IFREG:  printf("un fichier ordinaire\n");         break;
+    case S_IFSOCK: printf("un socket\n");                    break;
     default:       printf("inconnu ?\n");                 break;
     }
+    
+    ///Correction(partie 2)
+    char prot[9],*p;
+    p=prot;
+    sprintf(prot,"rwxrwxrwx ");
+    mask=1;
+    int i;
+    for(i=0;i<9;i++ )
+    {
+		if (buf.st_mode & mask == 0)
+			*p='-';
+		p++;
+		mask <<=1;
+	}
+	printf("protection %s\n",prot);
+    
+    /*
+    //montre la taille du fichier avec ST_SIZE (%ld ==> pour le type off_t)
+    printf("La taille du fichier est  %ld octet(s)\n",buf.st_size);
+    
+    switch (buf.st_mode) {
+		case S_IRWXU: printf("Le proprietaire à tous les droits sur le fichier\n"); break;
+		case S_IRUSR: printf("Le propriétaire a le droit de lecture \n"); break;
+		case S_IWUSR: printf("Le proprietaire a le droit d'ecriture\n"); break;
+		default:	  printf("Le proprietaire n'a aucun droit sur le fichier\n"); break;
+	}
 	
+	printf("Mode : %lo (octal)\n",(unsigned long) buf.st_mode);
+	*/
 	return 1;
 }
 
