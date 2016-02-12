f975dc1acc9329c048cf78fbba28f6c9094ac59e
yobero <yoannb94@gmail>
2016-02-12 19:10:20 +0100

    td2/4

---
+++ td2/clientServeur/client.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class client here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class client
+{
+    // instance variables - replace the example below with your own
+    private String nom;
+
+    /**
+     * Constructor for objects of class client
+     */
+    public client()
+    {
+        // initialise instance variables
+        this.nom = nom;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public client seConnecter(serveur Serveur)
+    {
+        
+    }
+}
