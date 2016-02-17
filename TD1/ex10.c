d9a828c97478571908dfcddd2b0ca4f6b9a4f943
yobero <yoannb94@gmail.com>
2016-02-17 13:49:20 +0100

    ex10

---
+++ TD1/ex10.c
@@ -0,0 +1,19 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+void separe(char* chaine,char*b[])
+{
+	int n=0;
+	while(b[n]
+}
+
+int main()
+{
+	
+	
+	return 1;
+}
