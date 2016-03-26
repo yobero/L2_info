f7ec0c1727971b65d269ddb4cc28c9ceecba852c
yobero <yoannb94@gmail.cam>
2016-03-26 10:51:18 +0100

    ex2 en cours (2)

---
+++ td6/ex2/chaineRestaurant.java
@@ -0,0 +1,33 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class chaineRestaurant
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private String nom;
+    private List<restaurant> chaine;
+
+    /**
+     * Constructeur d'objets de classe chaineRestaurant
+     */
+    public chaineRestaurant(String nom)
+    {
+       this.nom = nom;
+       this.chaine = new ArrayList<restaurant>();
+    }
+
+    public void ajoutRestarant(restaurant r)
+    {
+        this.chaine.add(r);
+    }
+    
+    public int taille()
+    {
+        return chaine.size();
+    }
+    
+    public restaurant getRestaurant(int i)
+    {
+        return chaine.get(i);
+    }
+}
---
+++ td6/ex2/reservation.java
@@ -0,0 +1,18 @@
+
+public class reservation
+{
+    // variables d'instance - remplacez l'exemple qui suit par le vôtre
+    private int x;
+
+    public reservation()
+    {
+        // initialisation des variables d'instance
+        x = 0;
+    }
+
+    public int sampleMethod(int y)
+    {
+        // Insérez votre code ici
+        return x + y;
+    }
+}
