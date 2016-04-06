e569966a6a2d913948aef526eafaabfd2ea4a219
yobero <yoannb94@gmail.com>
2016-04-06 09:49:05 +0200

    ex1 terminer3

--- td5/ex1.c
+++ td5/ex1.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <time.h>
+#include <memory.h>
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -9,15 +10,38 @@
 
 void copie (int s,int d) {
 	int n;
-	char[SIZE_BUF];
+	char buf[SIZE_BUF];
 	while ((n=read(s,buf,SIZE_BUF))>0) {
-		if (write(d,buf,n != n) {
+		if (write(d,buf,n) != n) {
 			perror("Impossible d'écrire");
 			exit(EXIT_FAILURE);
 		}
+		if(n==1 && buf[0] == '\n') //condition d'arrêt
+			return;
 	}
 	if(n!=0) {
 		perror("Impossible de lire");
 		exit(EXIT_FAILURE);
 	}
+}
+
+int main()
+{
+	int tab[2];
+	int pid;
+	pipe(tab);
+		
+	pid =fork();
+	
+	if(pid) {//lire l'info
+		close(tab[0]);
+		copie(0,tab[1]);
+		close(tab[1]);
+	}
+	else {//afficher l'info
+		close(tab[1]);
+		copie(tab[0],1);
+		close(tab[0]);
+	}
+	return 1;
 }
