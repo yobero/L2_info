2dfadc15478d494b655b874f4ab31569c921dc87
yobero <yoannb94@gmail.cam>
2016-04-07 00:12:57 +0200

    restaurant en cours

--- td6/ex2/reservation.java
+++ td6/ex2/reservation.java
@@ -1,16 +1,33 @@
+import java.util.Scanner;
 
 public class reservation
 {
-    // variables d'instance - remplacez l'exemple qui suit par le vôtre
 
-    public reservation()
+    public static void main() throws Exception
     {
-        // initialisation des variables d'instance
-        x = 0;
-    }
-
-    public static void main()
-    {
+        try {
+            Scanner scanner = new Scanner(System.in);
         
+            System.out.println("Choisissez un nom pour la chaine de restaurant");
+            String nomChaine = scanner.nextLine();
+            chaineRestaurant chaine = new chaineRestaurant(nomChaine);
+        
+            System.out.println("Combien de restaurant voulez-vous ?");
+            int nbRest = scanner.nextInt();
+            
+            if(nbRest<0)
+                throw new IllegalArgumentException("nbRest ne doit pas être négative !");
+                
+            int i=0;
+            while(i<nbRest)
+            {
+                restaurant rest = new restaurant() //valeur aléatoire
+                
+                i++;
+            }
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("ERREUR : "+e);
+        }
     }
 }
