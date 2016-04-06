67b75d84de5d96befcb9aeb5b9293c4d2fa5de07
yobero <yoannb94@gmail.com>
2016-04-06 09:04:51 +0200

    ajout td5/

---
+++ td5/ex1.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+#define SIZE_BUF 1024
+
+void copie (int s,int d) {
+	int n;
+	char[SIZE_BUF];
+	while ((n=read(s,buf,SIZE_BUF))>0) {
+		if (write(d,buf,n != n) {
+			
+		}
+	}
+}
