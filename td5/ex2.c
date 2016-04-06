9b0cee2f4fc23fa0a93de78a2632868c6338d7f3
yobero <yoannb94@gmail.com>
2016-04-06 11:14:25 +0200

    ex2 terminer

--- td5/ex2.c
+++ td5/ex2.c
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 
 #define SIZE_BUF 1024
-#define N 100
+#define N 10
 
 void copie (int s,int d) {
 	int n;
@@ -28,21 +28,34 @@ void copie (int s,int d) {
 
 int main()
 {
-	int tab[2];
-	pid_t pid[N];
-	pipe(tab);
-		
-	pid =fork();
+	int tab1[2];
+	int tab2[2];
+	pid_t pid;
+	int i=0;
 	
-	if(pid) {//lire l'info
-		close(tab[0]);
-		copie(0,tab[1]);
-		close(tab[1]);
+	tab1[0]=0;
+	tab1[1]=-1;
+	
+	while(i<N)
+	{ 
+		pipe(tab2);
+		if(fork()==0)
+		{
+			close(tab2[0]);
+			copie(tab1[0],tab2[1]);
+			close(tab1[0]);
+			close(tab2[1]);
+			exit(0);
+		}
+		close(tab1[0]);
+		close(tab1[1]);
+		close(tab2[1]);
+		tab1[0] = tab2[0];
+		i++;
 	}
-	else {//afficher l'info
-		close(tab[1]);
-		copie(tab[0],1);
-		close(tab[0]);
-	}
+	copie(tab1[0],1);
+
+
 	return 1;
+
 }
