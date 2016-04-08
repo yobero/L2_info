8fe4d3b83cce7da2f041587f5f5ee5d0e939f3b0
yobero <yoannb94@gmail.com>
2016-04-09 00:45:57 +0200

    BUG (2)

---
+++ td2/clientServeur/serveur.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class serveur here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class serveur
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class serveur
+     */
+    public serveur()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    /**Test si le client est connecter*/
+    
+    /**relit envoyer et recevoir du client*/
+    public boolean connecter(client Client)
+    {
+        serveur.connecter(Client);
+    }
+}
